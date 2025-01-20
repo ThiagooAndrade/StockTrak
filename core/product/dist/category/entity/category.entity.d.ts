d438f11aba53f9ebc429cd00c2c71528beb6faa8
ThiagooAndrade <andradethiago1609@gmail.com>
2025-01-20 22:00:21 +0000

    att

--- core/product/dist/category/entity/category.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Product } from "src/product/entity/product.entity";
-import { BaseEntity } from "typeorm";
-export declare class Category extends BaseEntity {
-    id: number;
-    nome: string;
-    categoria_pai: number;
-    filhos: Category[];
-    produtos: Product[];
-}
