9ebbe89b14d60329939dad471ceb6e1bd1918b9a
ThiagooAndrade <andradethiago1609@gmail.com>
2025-01-27 10:46:58 -0300

    teste

--- core/product/dist/modules/product/product.providers.d.ts
+++
@@ -1,7 +0,0 @@
-import type { DataSource } from "typeorm";
-import { Product } from "./product.entity";
-export declare const productProviders: {
-    provide: string;
-    useFactory: (dataSource: DataSource) => import("typeorm").Repository<Product>;
-    inject: string[];
-}[];
--- core/product/dist/modules/product/product.service.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Product } from "src/modules/product/product.entity";
-import type { Repository } from "typeorm";
-export declare class ProductService {
-    private productRepository;
-    constructor(productRepository: Repository<Product>);
-    findAll(): Promise<Product[]>;
-    hello(): string;
-}
